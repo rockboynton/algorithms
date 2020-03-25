d633828953154063f63849250b582419db4504d8
Rock Boynton <boyntonrl@msoe.edu>
2020-03-25 12:17:24 -0500

    Add stub files for lab2

---
+++ lab2/sorting.py
@@ -0,0 +1,32 @@
+def insertion_sort(lst):
+    """
+    Sorts the list lst in place using insertion_sort.
+    """
+    pass
+
+
+def merge_sort(lst, p, r):
+    """
+    Sorts the list lst in place using merge_sort.
+
+    Args:
+        lst: list to be sorted
+        p: starting index of the list, i.e 0
+        r: ending index of the list, i.e. len(lst) - 1
+    """
+    pass
+
+
+def merge(lst, p, q, r):
+    """
+    Merges two sorted partitions of lst.  This is
+    performed in-place. It is assumed that the
+    two partitions are contiguous in the list.
+
+    Args:
+        lst: list that is being sorted
+        p: starting index of first partition
+        q: ending index of first partition
+        r: ending index of second partition
+    """
+    pass
